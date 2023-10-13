0b5e6fd97f16a338996db39de11b1bbe28d50ed9
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-13 13:03:35 +0530

    Adding User,Email,Ticket details

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main(){
+	var conferenceName = "Go conference" 
+	const conferenceTicket uint = 50
+	var remainingTickets int = 50 
+
+	fmt.Println("welcome to the", conferenceName, "booking application")
+	fmt.Println("Number of tickets available is", remainingTickets)
+
+	var firstName,lastName,email string
+	var userTicket int
+	
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email details:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTicket)
+
+	remainingTickets = remainingTickets - userTicket
+
+	fmt.Printf("Thanks You!! %v %v for booking %v tickets. You will recive a confirmation email at %v\n", firstName, lastName, userTicket, email)
+
+	fmt.Printf("%v tickets left now\n", remainingTickets)
+
+}
+
