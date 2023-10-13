006d807bd02acdd674c053361d9cd098df9f7e51
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-13 17:38:03 +0530

    improved if else logic

--- main.go
+++ main.go
@@ -31,36 +31,38 @@ func main(){
 
 		if userTicket > int(remainingTickets){
 			fmt.Printf("We've only %v ticket left, you can not book %v tickets.\n", remainingTickets, userTicket)
-			break
-		}
-
-		remainingTickets = remainingTickets - uint(userTicket)
+		}else {
+			remainingTickets = remainingTickets - uint(userTicket)
 		//bookings[0] = firstName + " " + lastName 
-		bookings = append(bookings, firstName + " " + lastName)
+			bookings = append(bookings, firstName + " " + lastName)
 
-		fmt.Printf("Thanks You!! %v %v for booking %v tickets. You will recive a confirmation email at %v\n", firstName, lastName, userTicket, email)
+			fmt.Printf("Thanks You!! %v %v for booking %v tickets. You will recive a confirmation email at %v\n", firstName, lastName, userTicket, email)
 
-		fmt.Printf("%v tickets left now\n", remainingTickets)
+			fmt.Printf("%v tickets left now\n", remainingTickets)
 
-		var firstNames []string
+			var firstNames []string
 
-		for _, booking := range bookings{
-			// range provides the index and value of that element 
-			// range iterate over elemnt for different data structure
-			// blank identifier '_' for unused variable 
-			fmt.Printf("These are all our bookings:%v\n", booking)
-			var names = strings.Fields(booking) 
-			// split the string with white space as separator 
-			// and return a slice with split elements
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("These are all list of firstName from our bookings: %v\n", firstNames)
+			for _, booking := range bookings{
+				// range provides the index and value of that element 
+				// range iterate over elemnt for different data structure
+				// blank identifier '_' for unused variable 
+				fmt.Printf("These are all our bookings:%v\n", booking)
+				var names = strings.Fields(booking) 
+				// split the string with white space as separator 
+				// and return a slice with split elements
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("These are all list of firstName from our bookings: %v\n", firstNames)
 
-		// when reached 0 we need to end the application code 
-		if remainingTickets == 0{
-			//exit the code
-			fmt.Println("All ticket has been sold out, come next year")
-			break
+			// when reached 0 we need to end the application code 
+			if remainingTickets == 0{
+				//exit the code
+				fmt.Println("All ticket has been sold out, come next year")
+				break
 		}
 	}
+	}
 }
+
+
+		
