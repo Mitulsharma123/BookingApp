4b616e0ddb68c000e8c9aeed5ee14d9d38f2e6da
Mitulsharma123 <sharmamitul72@gmail.com>
2023-10-13 14:48:22 +0530

    adding logic for slice

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main(){
 	var conferenceName = "Go conference" 
 	const conferenceTicket uint = 50
 	var remainingTickets int = 50 
+	var bookings []string
 
 	fmt.Println("welcome to the", conferenceName, "booking application")
 	fmt.Println("Number of tickets available is", remainingTickets)
@@ -26,10 +27,14 @@ func main(){
 	fmt.Scan(&userTicket)
 
 	remainingTickets = remainingTickets - userTicket
+	//bookings[0] = firstName + " " + lastName 
+	bookings = append(bookings, firstName + " " + lastName)
 
 	fmt.Printf("Thanks You!! %v %v for booking %v tickets. You will recive a confirmation email at %v\n", firstName, lastName, userTicket, email)
 
 	fmt.Printf("%v tickets left now\n", remainingTickets)
+
+	fmt.Printf("These are all our bookings: %v \n", bookings)
 
 }
 
